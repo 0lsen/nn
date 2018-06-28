909a2a90428cd8e41f48de1c28481a4438d16d87
o.stepan <o.stepan@axilaris.de>
2018-06-28 17:03:20 +0200

    remove obsolete InputNeuron

--- src/NN/Model/InputLayer.php
+++ src/NN/Model/InputLayer.php
@@ -7,22 +7,10 @@ use NN\Builder\LayerConfiguration;
 
 class InputLayer extends Layer
 {
-	public function __construct(LayerConfiguration $configuration)
-	{
-		$this->neurons[] = new BiasNeuron();
-		for ($i = 0; $i < $configuration->numberOfNeurons; $i++) {
-			$this->neurons[] = new InputNeuron();
-		}
-	}
+	public function __construct(LayerConfiguration $configuration){}
 
 	public function calculate($input)
 	{
-		$output = [];
-
-		foreach ($this->neurons as $index => $neuron) {
-			$output[] = $neuron->output($index ? [$input[$index-1]] : null);
-		}
-
-		return $output;
+		return array_merge([1], $input);
 	}
 }
